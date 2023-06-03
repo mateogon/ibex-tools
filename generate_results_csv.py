e5853bfec1ce6ef57833be9b67bb0edd999c88b2
mateogon <mateogon97@hotmail.com>
2023-06-03 13:29:23 -0400

    added result metrics and readme

--- generate_results_csv.py
+++ generate_results_csv.py
@@ -17,110 +17,104 @@ This script works by:
     2. 'All Parameter Configurations': Listing each parameter configuration with its average improvements (best and overall) and how often it was the best.
 
 The output file 'combined_results_data.csv' will be written to the same directory where the script is located.
+
+
+ The dataframe best_params_per_file is added to the CSV file with the following columns:
+ 'file': The file name of the test case.
+ 'parameters': The configuration parameters extracted from the file name.
+ 'avg_time_best': The average time of the best performing configuration for the test case.
+ 'avg_time_baseline': The average time of the baseline for the test case.
+ 'improvement_best': The improvement percentage of the best configuration over the baseline for the test case.
+
+best_params_per_file = merged_df[['file', 'parameters', 'avg_time_best', 'avg_time_baseline', 'improvement_best']]
+
+ Rest of your code...
+
+ The dataframe merged_param_df is added to the CSV file with the following columns:
+ 'parameters': The configuration parameters.
+ 'count_best': The number of times the parameters configuration was the best.
+ 'mean_improvement_best': The mean improvement of the best cases for this configuration.
+ 'std_dev_improvement_best': The standard deviation of the improvement of the best cases for this configuration.
+ 'min_improvement_best': The minimum improvement of the best cases for this configuration.
+ 'max_improvement_best': The maximum improvement of the best cases for this configuration.
+ 'mean_improvement_all': The mean improvement of all cases for this configuration.
+ 'std_dev_improvement_all': The standard deviation of the improvement of all cases for this configuration.
+ 'min_improvement_all': The minimum improvement of all cases for this configuration.
+ 'max_improvement_all': The maximum improvement of all cases for this configuration.
+ 'total_time_improvement': The total time improvement for this configuration.
+
 """
 
 import os
 import pandas as pd
 import re
+import numpy as np
 
-def generate_results():
-    # Define the path where your CSVs are located
+def generate_results(num_runs_test, num_runs_baseline):
     csv_directory = os.path.dirname(os.path.abspath(__file__))
-
-    # Define the name of the output CSV file
     output_file = 'combined_results_data.csv'
-
-    # Initialize list to hold all data
     all_data = []
 
-    # Find the latest baseline file in the directory
     baseline_files = [f for f in os.listdir(csv_directory) if f.startswith("baseline_bench_data_")]
     latest_baseline_file = max(baseline_files, key=lambda f: os.path.getmtime(os.path.join(csv_directory, f)))
     baseline_path = os.path.join(csv_directory, latest_baseline_file)
-
-    # Read the baseline data
     baseline_data = pd.read_csv(baseline_path)
 
-    # Iterate over CSVs in the directory
     for filename in os.listdir(csv_directory):
         if filename.endswith(".csv"):
-            # Extract parameter configuration from the filename
             match = re.search('alpha.*', filename)
             if match is not None:
                 raw_parameters = match.group()
             else:
-                #print(f"No match in filename: {filename}")
                 continue
-            # Process parameters to make them more readable
             parameters = raw_parameters.replace('_', ' ').replace('.csv', '')
-
-            # Read the CSV
             df = pd.read_csv(os.path.join(csv_directory, filename))
-
-            # Add a column for the parameters
             df['parameters'] = parameters
-
-            # Append the DataFrame to the all_data list
             all_data.append(df)
 
-    # Concatenate all dataframes
     all_data_df = pd.concat(all_data, ignore_index=True)
-
-    # Merge all_data_df with the baseline_data DataFrame on the 'file' column
     all_merged_df = pd.merge(all_data_df, baseline_data, on='file', suffixes=('_test', '_baseline'))
-
-    # Calculate the improvement and add it as a new column for all data
     all_merged_df['improvement'] = 100 * (all_merged_df['avg_time_baseline'] - all_merged_df['avg_time_test']) / all_merged_df['avg_time_baseline']
-
-    # Find the parameters that resulted in the minimum average time for each file
     best_params_per_file = all_data_df.loc[all_data_df.groupby('file')['avg_time'].idxmin()]
-
-    # Merge the best_params_per_file DataFrame with the baseline_data DataFrame on the 'file' column
     merged_df = pd.merge(best_params_per_file, baseline_data, on='file', suffixes=('_best', '_baseline'))
-
-    # Calculate the improvement and add it as a new column
     merged_df['improvement_best'] = 100 * (merged_df['avg_time_baseline'] - merged_df['avg_time_best']) / merged_df['avg_time_baseline']
-
-    # Create a new DataFrame with the necessary columns
     best_params_per_file = merged_df[['file', 'parameters', 'avg_time_best', 'avg_time_baseline', 'improvement_best']]
 
-    # Count how often each parameter configuration is the best
-    param_counts = best_params_per_file['parameters'].value_counts()
+    # New metrics
+    all_merged_df['total_time_test'] = all_merged_df['avg_time_test'] * num_runs_test
+    all_merged_df['total_time_baseline'] = all_merged_df['avg_time_baseline'] * num_runs_baseline
+    all_merged_df['total_time_improvement'] = all_merged_df['total_time_baseline'] - all_merged_df['total_time_test']
+    total_time_per_param = all_merged_df.groupby('parameters')['total_time_improvement'].sum()
+    total_time_df = total_time_per_param.reset_index()
+    total_time_df.columns = ['parameters', 'total_time_improvement']
 
-    # Create a DataFrame from these counts
+    param_counts = best_params_per_file['parameters'].value_counts()
     param_counts_df = param_counts.reset_index()
     param_counts_df.columns = ['parameters', 'count_best']
 
-    # Calculate average improvement for each parameter configuration
-    all_param_improvements = all_merged_df.groupby('parameters')['improvement'].mean()
+    all_param_improvements = all_merged_df.groupby('parameters')['improvement'].agg([np.mean, np.std, np.min, np.max])
+    all_param_improvements.columns = ['mean_improvement_all', 'std_dev_improvement_all', 'min_improvement_all', 'max_improvement_all']
 
-    # Calculate average improvement for best parameter configuration
-    best_param_improvements = best_params_per_file.groupby('parameters')['improvement_best'].mean()
+    best_param_improvements = best_params_per_file.groupby('parameters')['improvement_best'].agg([np.mean, np.std, np.min, np.max])
+    best_param_improvements.columns = ['mean_improvement_best', 'std_dev_improvement_best', 'min_improvement_best', 'max_improvement_best']
 
-    # Merge the dataframes on 'parameters'
+    # Combine dataframes
     merged_param_df = pd.merge(best_param_improvements, all_param_improvements, left_index=True, right_index=True)
-
-    # Rename the columns
-    merged_param_df.columns = ['mean_best', 'mean_all']
-
-    # Merge with counts
     merged_param_df = merged_param_df.merge(param_counts_df, left_index=True, right_on='parameters')
-
-    # Reorder the columns
-    merged_param_df = merged_param_df[['parameters', 'count_best', 'mean_best', 'mean_all']]
-
-    # Sort the DataFrame by the count column in descending order
+    merged_param_df = merged_param_df.merge(total_time_df, on='parameters')
+    merged_param_df = merged_param_df[['parameters', 'count_best', 'mean_improvement_best', 'std_dev_improvement_best', 'min_improvement_best', 'max_improvement_best', 'mean_improvement_all', 'std_dev_improvement_all', 'min_improvement_all', 'max_improvement_all', 'total_time_improvement']]
     merged_param_df = merged_param_df.sort_values(by='count_best', ascending=False)
 
-    # Write best_params_per_file dataframe to csv
+    # After calculations, before writing to CSV
+    best_params_per_file = best_params_per_file.round(7)
+    merged_param_df = merged_param_df.round(7)
+
     with open(output_file, 'w') as f:
         f.write("\nBest Parameters per File\n")
     best_params_per_file.to_csv(output_file, mode='a', index=False)
 
-    # Write merged_param_df dataframe to csv
     with open(output_file, 'a') as f:
         f.write("\nAll Parameter Configurations\n")
     merged_param_df.to_csv(output_file, mode='a', index=False)
-
-
-
+    
+generate_results(5, 5)
